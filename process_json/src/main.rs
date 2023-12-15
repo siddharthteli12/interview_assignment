63b1bc45faf52d814619795caf5fbfbcd3e0224b
siddharthteli12 <telisiddharth123@yahoo.in>
2023-12-15 11:16:36 +0530

    Optimize: Read in buffer before writing

--- process_json/src/main.rs
+++ process_json/src/main.rs
@@ -1,21 +1,37 @@
 use std::{
     env,
     fs::File,
-    io::{self, stdin, stdout, BufReader, BufWriter, Read, Write},
+    io::{self, BufReader, BufWriter, Read, Write},
 };
 
-const BUFFER_SIZE: usize = 1024 * 8;
+const BUFFER_SIZE: usize = 8192;
 
-// Process json from input & write to output.
-pub fn process_json<R: Read, W: Write>(reader: R, mut writer: W) -> io::Result<()> {
-    for byte in reader.bytes().flatten() {
-        let result = if byte == b';' { b':' } else { byte };
-        writer.write(&[result])?;
+// Iter & replace byte for given buffer size.
+fn process_json_chunk(chunk: &[u8]) -> Vec<u8> {
+    chunk
+        .iter()
+        .map(|&byte| if byte == b';' { b':' } else { byte })
+        .collect()
+}
+
+// Process json from reader & write to writer.
+fn process_json<R: Read, W: Write>(mut reader: R, mut writer: W) -> io::Result<()> {
+    let mut buffer = [0; BUFFER_SIZE];
+
+    loop {
+        let bytes_read = reader.read(&mut buffer)?;
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let processed_chunk = process_json_chunk(&buffer[..bytes_read]);
+        writer.write_all(&processed_chunk)?;
     }
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() {
     // Collect cli args
     let args: Vec<String> = env::args().collect();
 
@@ -26,7 +42,7 @@ fn main() -> io::Result<()> {
             File::open(&args[2]).unwrap(),
         ))
     } else {
-        Box::new(BufReader::with_capacity(BUFFER_SIZE, stdin()))
+        Box::new(BufReader::with_capacity(BUFFER_SIZE, io::stdin()))
     };
 
     // Output is a file or stdout
@@ -36,9 +52,8 @@ fn main() -> io::Result<()> {
             File::create(&args[4]).unwrap(),
         ))
     } else {
-        Box::new(BufWriter::with_capacity(BUFFER_SIZE, stdout()))
+        Box::new(BufWriter::with_capacity(BUFFER_SIZE, io::stdout()))
     };
 
-    process_json(input, output)?;
-    Ok(())
+    process_json(input, output).expect("Unable to process json file");
 }
