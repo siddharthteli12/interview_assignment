d81b32d7078d218d5733352dd6313182e0dce293
siddharthteli12 <telisiddharth123@yahoo.in>
2023-12-15 10:57:48 +0530

    Refactor: Replace write_all with write
    Impl
    -Add comments
    -Change buffer size

--- process_json/src/main.rs
+++ process_json/src/main.rs
@@ -4,20 +4,22 @@ use std::{
     io::{self, stdin, stdout, BufReader, BufWriter, Read, Write},
 };
 
-const BUFFER_SIZE: usize = 1024 * 1024;
+const BUFFER_SIZE: usize = 1024 * 8;
+
+// Process json from input & write to output.
 pub fn process_json<R: Read, W: Write>(reader: R, mut writer: W) -> io::Result<()> {
     for byte in reader.bytes().flatten() {
-        if byte == b';' {
-            writer.write_all(&[b':']).expect("Unable to write to file");
-        } else {
-            writer.write_all(&[byte]).expect("Unable to write to file");
-        }
+        let result = if byte == b';' { b':' } else { byte };
+        writer.write(&[result])?;
     }
     Ok(())
 }
 
-fn main() {
+fn main() -> io::Result<()> {
+    // Collect cli args
     let args: Vec<String> = env::args().collect();
+
+    // Input is a file or stdin
     let input: Box<dyn Read> = if args.len() > 2 && args[1] == "--input" {
         Box::new(BufReader::with_capacity(
             BUFFER_SIZE,
@@ -27,6 +29,7 @@ fn main() {
         Box::new(BufReader::with_capacity(BUFFER_SIZE, stdin()))
     };
 
+    // Output is a file or stdout
     let output: Box<dyn Write> = if args.len() > 4 && args[3] == "--output" {
         Box::new(BufWriter::with_capacity(
             BUFFER_SIZE,
@@ -35,5 +38,7 @@ fn main() {
     } else {
         Box::new(BufWriter::with_capacity(BUFFER_SIZE, stdout()))
     };
-    process_json(input, output).expect("Unable to process json file");
+
+    process_json(input, output)?;
+    Ok(())
 }
